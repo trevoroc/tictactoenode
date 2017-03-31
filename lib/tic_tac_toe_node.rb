82279f0590ad3fad9f096296567ca43b94316033
Trevor OConnor <trevoroc22@gmail.com>
2017-03-31 15:43:37 -0700

    Implement #children, #get_empty_spaces, and #initialize for TicTacToeNode

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,26 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_list = []
+    empty_spaces = get_empty_spaces
+    empty_spaces.each do |pos|
+      new_board = @board.dup
+      new_board[pos] = @next_mover_mark
+      new_node_mark = @next_mover_mark == :x ? :o : :x
+      children_list << TicTacToeNode.new(new_board, new_node_mark, pos)
+    end
+    children_list
+  end
+
+  private
+
+  def get_empty_spaces
+    empty_spaces = []
+    @board.rows.each_with_index do |row, r|
+      row.each_index do |c|
+        empty_spaces << [r, c] if @board.empty?([r, c])
+      end
+    end
+    empty_spaces
   end
 end
